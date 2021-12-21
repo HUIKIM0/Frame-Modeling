03cb5d38f66daf4ff79262210af4de43fe7192d2
HUIKIM0 <fehs123@naver.com>
2021-12-21 18:39:11 +0900

    update

--- Frame Modeling/common/cGlobal.cs
+++ Frame Modeling/common/cGlobal.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Frame_Modeling    //namespace 동일하게. 전역으로 사용할
 {
@@ -21,7 +23,26 @@ namespace Frame_Modeling    //namespace 동일하게. 전역으로 사용할
         Error,
     }
 
+    public enum Kor_Week
+    {
+        월,
+        화,
+        수,
+        목,
+        금,
+        토,
+        일,
+    }
+
     //ucScreen1~3 의 delegate. Main으로 Log를 전달하기 위함
     public delegate void delLogSender(object oSender, enLogLevel eLevel, string strLog);
+
+    //ucScreen1의 delegate Main으로 Data값을 전달하기 위함
+    public delegate void delDataTableSender(object oSender, DataTable dt);
+    //ucScreen3의 delegate Main으로 Chart종류를 전달하기 위함
+    public delegate void delChartTypeSender(object oSender, SeriesChartType ct);
+
+
+
 
 }

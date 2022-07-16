ddd8e9abd790b1a950de71c3d111fabcf7dea2e7
HUIKIM0 <fehs123@naver.com>
2022-07-16 18:29:06 +0900

    .

--- Frame Modeling/common/cGlobal.cs
+++ Frame Modeling/common/cGlobal.cs
@@ -34,15 +34,17 @@ namespace Frame_Modeling    //namespace 동일하게. 전역으로 사용할
         일,
     }
 
-    //ucScreen1~3 의 delegate. Main으로 Log를 전달하기 위함
+    #region delegate 선언부분. event가 아닌, 선언이므로 여기에 써줘도OK 공통으로 가져갈것
+   
+    //ucScreen1~3 의 delegate. Main으로 Log를 전달
     public delegate void delLogSender(object oSender, enLogLevel eLevel, string strLog);
 
-    //ucScreen1의 delegate. Main으로 Data값을 전달하기 위함
+    //ucScreen1의 delegate. Main으로 Data값을 전달
     public delegate void delDataTableSender(object oSender, DataTable dt);
-    //ucScreen3의 delegate. Main으로 Chart종류를 전달하기 위함
+    //ucScreen3의 delegate. Main으로 Chart종류를 전달
     public delegate void delChartTypeSender(object oSender, SeriesChartType ct);
 
-
+    #endregion
 
     // Screen1/Screen3의 값을 가질 class
     public class ChartData
